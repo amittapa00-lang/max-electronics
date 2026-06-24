4feb29ed17f897f9a27858d8c5da64be7a2d7a32
amittapa00-lang <amitta.pa00@gmail.com>
2026-06-24 11:04:22 +0700

    Add sitemap

---
+++ app/sitemap.ts
@@ -0,0 +1,30 @@
+import { MetadataRoute } from "next";
+
+export default function sitemap(): MetadataRoute.Sitemap {
+  return [
+    {
+      url: "https://www.maxtechelectric.com",
+      lastModified: new Date(),
+      changeFrequency: "daily",
+      priority: 1,
+    },
+    {
+      url: "https://www.maxtechelectric.com/products",
+      lastModified: new Date(),
+      changeFrequency: "daily",
+      priority: 0.9,
+    },
+    {
+      url: "https://www.maxtechelectric.com/contact",
+      lastModified: new Date(),
+      changeFrequency: "monthly",
+      priority: 0.8,
+    },
+    {
+      url: "https://www.maxtechelectric.com/about",
+      lastModified: new Date(),
+      changeFrequency: "monthly",
+      priority: 0.8,
+    },
+  ];
+}
