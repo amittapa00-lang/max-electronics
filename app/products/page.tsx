b2a4e5e193cc3a285797ee8cdf561edcff8c087d
amittapa00-lang <amitta.pa00@gmail.com>
2026-07-02 09:05:24 +0700

    ISR

--- app/products/page.tsx
+++ app/products/page.tsx
@@ -3,6 +3,8 @@ import Link from "next/link";
 import Image from "next/image";
 import CategorySidebar from "@/components/CategorySidebar";
 
+export const revalidate = 300;
+
 export default async function ProductsPage({
   searchParams,
 }: {
